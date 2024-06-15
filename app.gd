e9b60a12999fbb63a9b8f7e1aeb9aaa2ae669f16
kip <shortbow123@users.noreply.github.com>
2024-06-15 15:37:02 -0700

    V0.4.0
    * All dates are now sorted by either weekly or biweekly
    * Updated UI
    * More work on settings backend
    * Better way of dealing with human time
    * Each entry has its own unique id

--- app.gd
+++ app.gd
@@ -1,8 +1,14 @@
 extends Control
 
 
-const papaEpoch = 1704139200
+# 1/1/2025 : 1735718400
+# 31622400 366 days
+#const papaEpoch = 1718607600 # 6/17/2024
+const papaEpoch = 1717398000 # 6/3/2024 
 const periodLength = 2419200
+const biWeekly = 1209600 #2 seconds before midnight 2 weeks from before
+const weekly = biWeekly / 2
+
 
 var periods2024 = {
 	1: 1704139200,
@@ -11,6 +17,7 @@ var periods2024 = {
 	4: 1710745200,
 	5: 1713769200,
 	6: 1716188400,
+	
 	7: 1718607600,
 	8: 1720422000,
 	9: 1723446000,
@@ -18,31 +25,78 @@ var periods2024 = {
 	11: 1727679600,
 	12: 1730098800,
 }
+var alphabet = ['A', 'B', 'C', 'D','E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var versionInfo = "Alpha "
-var revisionNumber = "0.2.0 "
-var revisionType = "A " # A=Android I=IOS D=Debug
+var revisionNumber = "0.3.0 "
+var revisionType = " " # A=Android I=IOS D=Debug
 var popupFor = ""
 var entries = []
+var sortedEntries = []
 var loadedSave
 var currentPeriod = 0
 var clearSave = false
 var whichEntrySelected = 0
-# datecode is how many days since jan 1st 2024
-# dummy entry
-# [hoursworked, insidetips, drivertips, hours driven]
+var whichUUIDSelected = null
+var selectedPayView = "biweekly"
+var payStart = 1718607600
+var payInterval = 0
+var random = RandomNumberGenerator.new()
 
-func selectSave():
-	var whichSave
-	var save_game = File.new()
-	save_game.open("user://runSave.save", File.READ)
-	while save_game.get_position() < save_game.get_len():
-		whichSave = int(save_game.get_line())
-	save_game.close()
+func setSelectedPayStart(whichInterval):
+	selectedPayView = whichInterval
+	if selectedPayView == "biweekly":
+		$canvas/popup/reviewView/biweekly.disabled = true
+		$canvas/popup/reviewView/weekly.disabled = false
+	elif selectedPayView == "weekly":
+		$canvas/popup/reviewView/biweekly.disabled = false
+		$canvas/popup/reviewView/weekly.disabled = true
+	var currentPayWeek = papaEpoch
+	if whichInterval == "biweekly":
+		payInterval = biWeekly
+	elif whichInterval == "weekly":
+		payInterval = weekly
+	while true:
+		if returnTime() > currentPayWeek and returnTime() < currentPayWeek + payInterval:
+			payStart = currentPayWeek
+			break
+		else:
+			currentPayWeek += payInterval
+	print(payStart)
+
+func changePayStart(increase):
+	if increase:
+		payStart += payInterval
+	else:
+		payStart -= payInterval
+	$canvas/popup/reviewView/dates.text = str(unixToHumanTime(payStart)) + " to " + str(unixToHumanTime(payStart + payInterval))
+	reviewData()
+
+func returnSortedDates():
+	sortedEntries = []
+	if selectedPayView == "biweekly":
+		for entry in entries:
+			print(entry["date"])
+			print(payStart)
+			print(payStart + biWeekly)
+			if entry["date"] > payStart and entry["date"] < payStart + biWeekly:
+				sortedEntries.append(entry)
+	elif selectedPayView == "weekly":
+		for entry in entries:
+			if entry["date"] > payStart and entry["date"] < payStart + weekly:
+				sortedEntries.append(entry)
+
+func setPayCycleText():
+	$canvas/popup/reviewView/dates.text = unixToHumanTime(payStart) + " - " + unixToHumanTime(payStart + payInterval)
+
+func unixToHumanTime(unixTime):
+	return(Time.get_date_dict_from_unix_time(unixTime))
 
 func saveData():
 	var saveLocation = "user://papaFile.save"
 	var save_dict = {
-		"saveVersion" : 1,
+		"saveVersion" : 2,
+		"clearSave": clearSave,
 		"savedEntries": entries
 	}
 	print(saveLocation)
@@ -60,30 +114,34 @@ func loadData():
 	save_game.open(saveLocation, File.READ)
 	loadedSave = parse_json(save_game.get_line())
 	save_game.close()
-	if loadedSave["saveVersion"] == 1:
-		#upgrade database code here
-		pass
+	if loadedSave["saveVersion"] == 1: #Erases any version 1 data
+		print("----------------")
+		print("CLEARING ALL DATA")
+		print("----------------")
+		entries = [] 
+		saveData()
 	loadVariables()
 
 func loadVariables(): 
 	entries = loadedSave["savedEntries"]
 
 func _ready():
-	$canvas/homeScreen/version.text = str(versionInfo + revisionNumber + revisionType)
-	setCurrentPeriod()
-	setScreen("homeScreen")
-	if clearSave:
-		print("----------------")
-		print("CLEARING SAVE")
+	if clearSave:  #this works bc there is no data loaded to the variables so i call it to save and it overwrites data
+		print("----------------") #in the save file, if clear save is not true it loads the data like normal
+		print("CLEARING ALL DATA") #might have it backup data before saving but that is a problem for future me
 		print("----------------")
 		saveData()
 	else:
 		loadData()
-	
+	$canvas/homeScreen/version.text = str(versionInfo + revisionNumber + revisionType)
+	random.randomize()
+	#setCurrentPeriod()
+	setSelectedPayStart("biweekly")
+	returnSortedDates()
+	setScreen("homeScreen")
 
-func returnEpoch():
+func returnTime():
 	return(Time.get_unix_time_from_datetime_dict(Time.get_datetime_dict_from_system()))
-
 
 func setScreen(screenToShow):
 	for screen in get_tree().get_nodes_in_group("screen"):
@@ -95,74 +153,150 @@ func popupConfirm(pressed):
 	if popupFor == "recordHours":
 		if pressed:
 			recordData("hours", $canvas/popup/recordHours/select.value)
+		endPopup()
+	elif popupFor == "recordTravel":
+		if pressed:
+			recordData("travel", $canvas/popup/recordHours/select.value)
+		endPopup()
+	elif popupFor == "editHours":
+		var whichHours = ""
+		if not entries[whichEntrySelected]["hoursWorked"] == 0:
+			whichHours = "hoursWorked"
 		else:
-			pass
-		popupFor = ""
-		$canvas/popup.visible = false
-		$canvas/popup/recordHours.visible = false
+			whichHours = "travelHours"
+		if pressed:
+			entries[whichEntrySelected][whichHours] = $canvas/popup/recordHours/select.value
+			saveData()
+		reviewData()
+		endPopup()
+	elif popupFor == "deleteHours":
+		if pressed:
+			sortedEntries.remove(whichEntrySelected)
+			print(len(entries))
+			for i in len(entries):
+				if entries[i]["UUID"] == whichUUIDSelected:
+					entries.remove(i)
+					saveData()
+		reviewData()
+		endPopup()
+
+func endPopup():
+	popupFor = ""
+	$canvas/popup.visible = false
+	$canvas/popup/recordHours.visible = false
 
 func popupControl(whatPopup):
 	$canvas/popup.popup()
+	$canvas/popup/recordHours.visible = true
+	$canvas/popup/recordHours/select.value
+	$canvas/popup/recordHours/select.visible = true
+	$canvas/popup/reviewView.visible = false
 	if whatPopup == "recordHours":
 		$canvas/popup.window_title = "Record hours"
 		$canvas/popup/header.text = "Enter hours worked today:"
 		popupFor = "recordHours"
-		$canvas/popup/recordHours.visible = true
+	elif whatPopup == "recordTravel":
+		$canvas/popup.window_title = "Record travel hours"
+		$canvas/popup/header.text = "Enter hours traveled today:"
+		popupFor = "recordTravel"
+	elif whatPopup == "editHours":
+		$canvas/popup.window_title = "Edit hours"
+		$canvas/popup/header.text = "Edit hours"
+		popupFor = "editHours"
+	elif whatPopup == "deleteHours":
+		$canvas/popup.window_title = "Delete Hours"
+		$canvas/popup/header.text = "Remove timeclock record?"
+		popupFor = "deleteHours"
+		$canvas/popup/recordHours/select.visible = false
+	elif whatPopup == "changeView":
+		$canvas/popup.window_title = "Change view"
+		$canvas/popup/header.text = "Select pay increment?"
+		popupFor = "changeView"
+		$canvas/popup/reviewView.visible = true
+		$canvas/popup/recordHours.visible = false
+	else:
+		print("WRONG popupControl USAGE ", whatPopup)
+	
 
 func recordData(recordWhat, value):
 	var newEntry = {
+		"UUID": 0,
 		"hoursWorked": 0,
 		"insideTips": 0,
 		"driverTips": 0,
 		"travelHours": 0,
 		"editTime": 0,
 		"date": 0,
-		"period": 0
+		"period": 0,
+		"store": 0,
+		"RV1" : null,
+		"RV2" : null,
+		"RV3" : null,
+		"RV4" : null
 	}
+	newEntry["UUID"] = randomUUID()
 	if recordWhat == "hours":
-		newEntry["editTime"] = returnEpoch()
-		newEntry["date"] = returnEpoch()
+		newEntry["editTime"] = returnTime()
+		newEntry["date"] = returnTime()
 		newEntry["hoursWorked"] = $canvas/popup/recordHours/select.value
 		newEntry["period"] = currentPeriod
-	entries.append(newEntry)
+	elif recordWhat == "travel":
+		newEntry["editTime"] = returnTime()
+		newEntry["date"] = returnTime()
+		newEntry["travelHours"] = $canvas/popup/recordHours/select.value
+		newEntry["period"] = currentPeriod
+	if $canvas/popup/recordHours/select.value > 0:
+		entries.append(newEntry)
 	print(entries)
 	saveData()
 
-func setCurrentPeriod():
+func randomUUID():
+	var tempRandom = 0
+	var uuidString = ""
+	for i in range(11):
+		tempRandom = random.randi_range(0, 1)
+		if tempRandom == 0:
+			uuidString += str(alphabet[random.randi_range(0, 25)])
+		else:
+			uuidString += str(random.randi_range(0, 9))
+	return(uuidString)
+
+func setCurrentPeriod(): #probably can remove
 	var periodR = 1
 	var tempEpoch = papaEpoch
-	var currentEpoch = returnEpoch()
+	var currentEpoch = returnTime()
 	while true:
 		if periods2024[periodR] < currentEpoch and periods2024[periodR + 1] >= currentEpoch: 
 			currentPeriod = periodR
 			print(currentPeriod)
 			break
 		else:
-			#print(currentEpoch)
-			#print(periods2024[periodR])
 			periodR += 1
 
 func reviewData():
 	var totalHours = 0
-	#var testt = Time.get_date_string_from_unix_time(entry["date"]) + ": " + str(entry["hoursWorked"])
+	returnSortedDates()
 	for i in range($canvas/reviewData/list.get_item_count()):
 		$canvas/reviewData/list.remove_item(0)
-	for entry in entries:
-		$canvas/reviewData/list.add_item((Time.get_date_string_from_unix_time(entry["date"]) + ": " + str(entry["hoursWorked"])))
-		totalHours += entry["hoursWorked"]
-		#var newas = (testt(entry["date"]))
-		# + ": " + str(entry[hoursWorked])))
+	for entry in sortedEntries:
+		if entry["hoursWorked"] > 0:
+			$canvas/reviewData/list.add_item((Time.get_date_string_from_unix_time(entry["date"]) + " - Work Hours: " + str(entry["hoursWorked"])))
+			totalHours += entry["hoursWorked"]
+		elif entry["hoursWorked"] == 0:
+			$canvas/reviewData/list.add_item((Time.get_date_string_from_unix_time(entry["date"]) + " - Travel Hours: " + str(entry["travelHours"])))
+			totalHours += entry["travelHours"]
 	$canvas/reviewData/totalbg/total.text = "Total Hours: " + str(totalHours)
 
 func reviseData(function):
 	if function == "edit":
-		pass
+		popupControl("editHours")
+		$canvas/popup/recordHours/select.value
 	elif function == "delete":
-		pass
-
+		popupControl("deleteHours")
 
 func selectEntryToEdit(index):
 	toggleEditButtons(true)
+	whichUUIDSelected = sortedEntries[index]["UUID"]
 	whichEntrySelected = index
 	
 func toggleEditButtons(enable):
@@ -198,17 +332,30 @@ func _on_review_pressed():
 func _on_list_item_selected(index):
 	selectEntryToEdit(index)
 
-
 func _on_list_nothing_selected():
 	whichEntrySelected = null
 	toggleEditButtons(false)
 
-
 func _on_edit_pressed():
 	reviseData("edit")
-	pass # Replace with function body.
-
 
 func _on_delete_pressed():
 	reviseData("delete")
-	pass # Replace with function body.
+
+func _on_recordTravel_pressed():
+	popupControl("recordTravel")
+
+func _on_biweekly_pressed():
+	setSelectedPayStart("biweekly")
+
+func _on_weekly_pressed():
+	setSelectedPayStart("weekly")
+
+func _on_back_pressed():
+	changePayStart(false)
+
+func _on_forward_pressed():
+	changePayStart(true)
+
+func _on_changeView_pressed():
+	popupControl("changeView")
